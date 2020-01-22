a5263ccdabfb0ae496db709dd626b04c648c1401
chenjiyuan <jiyuan.chen@easystack.cn>
2020-01-22 18:38:05 +0800

    Add search detail page

--- src/app/views/search/search-routing.module.ts
+++ src/app/views/search/search-routing.module.ts
@@ -7,15 +7,21 @@ import {SearchBoxModule} from '../../business/search-box/search-box.module';
 import {ScrollYModule} from '../../components/scroll-y/scroll-y.module';
 import {SearchHistoryModule} from '../../business/search-history/search-history.module';
 import {SearchListModule} from '../../business/search-list/search-list.module';
+import {DetailComponent} from './detail/detail.component';
 
 const routes: Routes = [{
   path: '',
-  component: SearchComponent
+  component: SearchComponent,
+  children: [{
+    path: ':id',
+    component: DetailComponent
+  }]
 }];
 
 @NgModule({
   declarations: [
-    SearchComponent
+    SearchComponent,
+    DetailComponent
   ],
   imports: [
     CommonModule,
@@ -27,7 +33,8 @@ const routes: Routes = [{
     SearchListModule
   ],
   exports: [
-    SearchComponent
+    SearchComponent,
+    DetailComponent
   ]
 })
 export class SearchRoutingModule { }
